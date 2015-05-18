d9face8aeaa04dd8196f0ed062d8644f3225f938
dsk0701 <d.dubandtribal@gmail.com>
2015-05-18 20:03:32 +0900

    Refactor and add testing.

--- backup.sh
+++ backup.sh
@@ -5,23 +5,23 @@ CONTAINER_BACKUP_DIR=/backups
 BACKUP_FILE_NAME=`date +"%Y%m%d_%H%M"`
 HOST_BACKUP_DIR="$(pwd)/backup"
 
-STORAGE_CONTAINER_ID=`docker ps | grep storage | awk '{print $1}'`
+STORAGE_SERVICE_NAME="storage"
+STORAGE_CONTAINER_NAME=`docker-compose ps | grep ${STORAGE_SERVICE_NAME} | awk '{print $1}'`
 
 # コンテナを停止します。
 echo "Stopping containers..."
 docker-compose stop
 
 # バックアップを行います。
-docker run --rm --volumes-from ${STORAGE_CONTAINER_ID} -v ${HOST_BACKUP_DIR}:${CONTAINER_BACKUP_DIR} ubuntu tar zcvf ${CONTAINER_BACKUP_DIR}/backup-${BACKUP_FILE_NAME}.tar.gz ${CONTAINER_BACKUP_DATA_DIR}
+docker run --rm --volumes-from ${STORAGE_CONTAINER_NAME} -v ${HOST_BACKUP_DIR}:${CONTAINER_BACKUP_DIR} ubuntu tar zcvf ${CONTAINER_BACKUP_DIR}/backup-${BACKUP_FILE_NAME}.tar.gz ${CONTAINER_BACKUP_DATA_DIR}
 
 # 4世代残しておく。
 COUNT=0
-ls -1t ${HOST_BACKUP_DIR}/backup-* | while read LINE
+ls -1t ${HOST_BACKUP_DIR}/backup-* | while read BACKUP_FILE
 do
-    echo "LINE:${LINE}"
     if [ $COUNT -gt 3 ]; then
-        echo "Remove old backup file:${LINE}"
-        rm -f ${LINE}
+        echo "Remove old backup file:${BACKUP_FILE}"
+        rm -f ${BACKUP_FILE}
     fi
     COUNT=$((COUNT + 1))
 done
